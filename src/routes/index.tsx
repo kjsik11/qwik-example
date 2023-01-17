f8486e9a1b2d71eee37e4287ed364a9ac01e85e8
jongsik <kjsik11@gmail.com>
2023-01-17 17:14:52 +0900

    configuration unused import option to eslint

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { Link } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (
@@ -137,9 +136,6 @@ export default component$(() => {
           </a>
         </li>
       </ul>
-      <Link class="mindblow" href="/flower/">
-        Blow my mind 🤯
-      </Link>
     </div>
   );
 });
